9bddd277133c83aabd07e2803948c97fa58feabe
Wayaer <wayaer@foxmail.com>
2021-08-24 14:15:43 +0800

    no message

--- ios/Classes/FlWebviewPlugin.swift
+++ ios/Classes/FlWebviewPlugin.swift
@@ -2,14 +2,37 @@ import Flutter
 import UIKit
 
 public class FlWebviewPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "fl.webview", binaryMessenger:
-    registrar.messenger())
-    let instance = SwiftFlWebviewPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+    var channel: FlutterMethodChannel?
+    var webview: FlWebview?
+    var registrar: FlutterPluginRegistrar
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "fl.webview", binaryMessenger:
+            registrar.messenger())
+        let instance = FlWebviewPlugin(registrar, channel)
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    init(_ _registrar: FlutterPluginRegistrar, _ _channel: FlutterMethodChannel) {
+        channel = _channel
+        registrar = _registrar
+        super.init()
+    }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "initial":
+            if webview == nil {
+                webview = FlWebview(registrar.textures())
+                webview!.initial(call)
+            }
+            result(webview != nil)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
+    public func detachFromEngine(for registrar: FlutterPluginRegistrar) {
+        channel?.setMethodCallHandler(nil)
+        channel = nil
+    }
 }
--- lib/fl_webview.dart
+++ lib/fl_webview.dart
@@ -1,3 +1,47 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-const MethodChannel _channel = const MethodChannel('fl.webview');
+const MethodChannel _channel = MethodChannel('fl.webview');
+
+class FlWebView extends StatefulWidget {
+  const FlWebView({Key? key, required this.controller}) : super(key: key);
+  final FlWebViewController controller;
+
+  @override
+  _FlWebViewState createState() => _FlWebViewState();
+}
+
+class _FlWebViewState extends State<FlWebView> {
+  late FlWebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+      initial();
+    });
+  }
+
+  Future<void> initial() async {
+    controller = widget.controller;
+    final bool state = await controller.initial();
+    if (state) setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget current = const SizedBox();
+    if (controller.textureId != null)
+      current = Texture(textureId: controller.textureId!);
+    return SizedBox.expand(child: current);
+  }
+}
+
+class FlWebViewController extends ChangeNotifier {
+  int? textureId;
+
+  Future<bool> initial() async {
+    final bool? state = await _channel.invokeMethod<bool>('initial');
+    return state ?? false;
+  }
+}
